1eac6f41140089dcb54edcf3eb508906c3141b94
unknown <yise7171@naver.com>
2021-01-29 16:00:11 +0900

    #1.0 GraphQL Test 예제

---
+++ graphql/resolvers.js
@@ -0,0 +1,10 @@
+import { people, getById } from "./db.js";
+
+const resolvers = {
+    Query: {
+        people: () => people,
+        person: (_, { id }) => getById(id)
+    }
+};
+
+export default resolvers;
